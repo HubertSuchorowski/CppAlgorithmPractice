da5a73ad977aa12fde31db2691eab28c292c654e
Hubert Suchorowski <hsuchorowski@gmail.com>
2025-06-01 19:43:47 +0200

    Adds basic employee management class
    Initializes a simple Employee class with methods to set and retrieve employee details such as name, company, and age.
    
    Includes a method to introduce the employee.
    
    Creates instances of employees and demonstrates their introduction.

---
+++ OOPLearning/OOPLearning/OOPLearning.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using std::string;
+
+class Employee //blueprint for Employee objects
+{
+private:
+	string Name;
+	string Company;
+	int Age;
+public:
+	void SetName(string name) //method to set the name of the employee
+	{
+		Name = name;
+	}
+	string GetName()
+	{
+		return Name; //return the name of the employee
+	}//method to get the name of the employee
+
+	void SetCompany(string company) //method to set the company of the employee
+	{
+		Company = company;
+	}
+
+	string GetCompany()
+	{
+		return Company; //return the company of the employee
+	} //method to get the company of the employee
+
+	void SetAge(int age) //method to set the age of the employee
+	{
+		Age = age;
+	}
+
+	int GetAge()
+	{
+		return Age; //return the age of the employee
+	} //method to get the age of the employee
+
+	void Introduce () //method to introduce the employee
+	{
+		std::cout << "Hello, my name is " << Name << ", I work at " << Company << " and I am " << Age << " years old." << std::endl;
+	}
+
+	Employee(string name, string company, int age) {
+		Name = name;
+		Company = company;
+		Age = age;
+	}
+
+};
+
+int main()
+{
+	Employee employee1 = Employee("John Doe","Tech Support",25);//create an Employee objects
+	employee1.Introduce(); 
+	Employee employee2 = Employee("Jane Smith", "Software Development", 30);//create another Employee object
+	employee2.Introduce(); //call the Introduce method for the second employee
+}
+
